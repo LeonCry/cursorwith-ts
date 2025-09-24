100070394ca3e23e55072888f78c881dd7ce0cd8
voidis <lbh_ldu@outlook.com>
2025-09-24 09:07:27 +0800

    fix: :bug: 修复导入名称问题

--- apps/vue-test/src/views/test/index.vue
+++ apps/vue-test/src/views/test/index.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { CreateCursorWith } from '@cursor-with/core';
+import { CreateCursorWith } from 'cursorwith-core';
 
 const cursorWith = ref<InstanceType<typeof CreateCursorWith> | null>(null);
 onMounted(() => {
